f127ea9045f8e43c15cf4185233e4994cd679d4a
hossein-zare <zarehossein.hz1997@gmail.com>
2023-02-09 19:00:09 +0330

    Define UnsignedBigAutoField.

--- wester/models.py
+++ wester/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class UnsignedAutoField(models.AutoField):
+class UnsignedBigAutoField(models.BigAutoField):
     def db_type(self, connection):
         return 'bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT'
 
