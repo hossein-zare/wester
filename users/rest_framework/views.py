a93f0593ba8a50719f82773b29d0edc647b09c26
hossein-zare <zarehossein.hz1997@gmail.com>
2023-02-09 21:21:23 +0330

    Testing UserSerializer in view.

--- users/rest_framework/views.py
+++ users/rest_framework/views.py
@@ -2,10 +2,14 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 from wester.rest_framework.permissions import IsGuest
+from .serializers import UserSerializer
 
 @api_view()
 @permission_classes([IsGuest])
 def register(request):
-    return Response({
-        'name': 'hossein'
-    })
+    serializer = UserSerializer(data=request.data)
+
+    if not serializer.is_valid():
+        return Response(serializer.errors)
+
+    return Response(serializer.validated_data)
