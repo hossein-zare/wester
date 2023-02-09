6526ea5246b0fb0b472d8927dfbcfed36f582c67
hossein-zare <zarehossein.hz1997@gmail.com>
2023-02-09 17:02:42 +0330

    Fix fields.

--- users/models.py
+++ users/models.py
@@ -2,23 +2,28 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.utils import timezone
 from wester.fields import UnsignedAutoField
+from users.managers import UserManager
 
 class User(AbstractBaseUser):
     id = UnsignedAutoField(primary_key=True)
-    name = models.CharField(max_length=150, blank=True)
+    name = models.CharField(max_length=150, null=True)
     username = models.CharField(max_length=40, unique=True)
     email = models.EmailField(unique=True)
-    mobile_number = models.EmailField(unique=True, blank=True)
+    email_verified = models.BooleanField(default=False)
+    mobile_number = models.CharField(max_length=12, unique=True, null=True)
     is_staff = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_restricted = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
 
-    EMAIL_FIELD = "email"
-    USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["email"]
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
 
     def __str__(self):
         return self.name or self.username
