afdcc4ef4b1a6a4f6dfea4a625b916a68cc573f6
hossein-zare <zarehossein.hz1997@gmail.com>
2023-02-10 17:12:06 +0330

    Add get_client_ip_address.

--- wester/utils.py
+++ wester/utils.py
@@ -1,2 +1,15 @@
 def get_app_name(package):
     return package.rsplit('.', 1)[-1]
+
+# This function is considered dangerous
+# Any client can send a X_FORWARDED_FOR header manually with any value
+# So we first check if there's a valid for REMOTE_ADDR
+# Otherwise we're behind a reverse proxy and the value for X_FORWARDED_FOR must be safe
+# Which is forced by the proxy
+def get_client_ip_address(request):
+    ip = request.META.get('REMOTE_ADDR')
+
+    if len(ip) > 0:
+        return ip
+
+    return request.META.get('HTTP_X_FORWARDED_FOR').split(',')[0].strip()
