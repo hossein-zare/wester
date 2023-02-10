80b93b21bb33b53dbcb1a850b08b0edb2a42b3f8
hossein-zare <zarehossein.hz1997@gmail.com>
2023-02-11 00:07:55 +0330

    Define login view.

--- users/rest_framework/views.py
+++ users/rest_framework/views.py
@@ -1,15 +1,38 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from operator import itemgetter
 
 from wester.rest_framework.permissions import IsGuest
-from .serializers import UserSerializer
+from wester.utils import get_client_ip_address
+from .serializers import RegisterSerializer, LoginSerializer
+from ..models import User
+from .auth import create_token
 
-@api_view()
+@api_view(['POST'])
 @permission_classes([IsGuest])
 def register(request):
-    serializer = UserSerializer(data=request.data)
+    serializer = RegisterSerializer(data=request.data)
 
     if not serializer.is_valid():
         return Response(serializer.errors)
 
-    return Response(serializer.validated_data)
+    user = User.objects.create_user(**serializer.validated_data)
+    token = create_token(request, user).key
+
+    return Response({
+        'token': key
+    })
+
+@api_view(['POST'])
+@permission_classes([IsGuest])
+def login(request):
+    serializer = LoginSerializer(data=request.data)
+
+    if not serializer.is_valid():
+        return Response(serializer.errors)
+
+    token = create_token(request, serializer.user).key
+
+    return Response({
+        'token': token
+    })
