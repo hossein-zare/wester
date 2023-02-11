2add7b2209f78acfad50736f4af8221fd9b60494
hossein-zare <zarehossein.hz1997@gmail.com>
2023-02-11 14:48:25 +0330

    Minor.

--- wester/utils.py
+++ wester/utils.py
@@ -1,11 +1,13 @@
 def get_app_name(package):
     return package.rsplit('.', 1)[-1]
 
-# This function is considered dangerous
-# Any client can send a X_FORWARDED_FOR header manually with any value
-# So we first check if there's a valid for REMOTE_ADDR
-# Otherwise we're behind a reverse proxy and the value for X_FORWARDED_FOR must be safe
-# Which is forced by the proxy
+"""
+This function is considered dangerous
+Any client can send a X_FORWARDED_FOR header manually with any value
+So we first check if there's a valid for REMOTE_ADDR
+Otherwise we're behind a reverse proxy and the value for X_FORWARDED_FOR must be safe
+Which is forced by the proxy
+"""
 def get_client_ip_address(request):
     ip = request.META.get('REMOTE_ADDR')
 
