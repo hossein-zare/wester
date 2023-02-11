2e43f6a89dd3767296b57ce8bd3e0f9ce40fa60c
hossein-zare <zarehossein.hz1997@gmail.com>
2023-02-12 01:10:42 +0330

    Create create post view.

---
+++ posts/rest_framework/views.py
@@ -0,0 +1,21 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+from .serializers import PostSerializer
+from users.permissions import CanUser
+
+@api_view(['POST'])
+@permission_classes([CanUser('create_post')])
+def create(request):
+    """
+    Create a new post.
+    """
+
+    serializer = PostSerializer(data=request.data)
+
+    if not serializer.is_valid():
+        return Response(serializer.errors)
+
+    post = serializer.save(user=request.user)
+
+    return Response(serializer.data)
