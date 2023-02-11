4c6026306856727cb46b3734757827a18cb221fc
hossein-zare <zarehossein.hz1997@gmail.com>
2023-02-12 02:10:17 +0330

    Convert views to a single classed-based view.

--- posts/rest_framework/views.py
+++ posts/rest_framework/views.py
@@ -1,21 +1,35 @@
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from .serializers import PostSerializer
 from users.rest_framework.permissions import CanUser
+from ..models import Post
 
-@api_view(['POST'])
-@permission_classes([CanUser('create_post')])
-def create(request):
-    """
-    Create a new post.
-    """
+class Posts(APIView):
+    permission_classes = [CanUser('create_post', methods=['POST'])]
 
-    serializer = PostSerializer(data=request.data)
+    def get(self, request, id):
+        """
+        Read a post.
+        """
 
-    if not serializer.is_valid():
-        return Response(serializer.errors)
+        post = get_object_or_404(Post, pk=id)
+        serializer = PostSerializer(post)
 
-    serializer.save(user=request.user)
+        return Response(serializer.data)
 
-    return Response(serializer.data)
+    def post(self, request):
+        """
+        Create a new post.
+        """
+
+        serializer = PostSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors)
+
+        serializer.save(user=request.user)
+
+        return Response(serializer.data)
