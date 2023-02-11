00a2eea99a6760a4dbb33204021b5911bd6ad717
hossein-zare <zarehossein.hz1997@gmail.com>
2023-02-11 18:25:36 +0330

    Create a permission record for user on creation.

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,11 @@
+from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.conf import settings
 
+from .models import Permission
+
 # Example:
-# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-# def create_token():
-#     pass
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_permissions(sender, instance=None, created=False, **kwargs):
+    if created:
+        Permission.objects.create(user=instance)
