ba4826ea19fb3929057d27616c07078513ad45d1
hossein-zare <zarehossein.hz1997@gmail.com>
2023-02-12 01:19:46 +0330

    Remove rest_framework from the main app.

--- wester/rest_framework/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsGuest(BasePermission):
-    """
-    Allows access only to guest users (unauthenticated).
-    """
-
-    def has_permission(self, request, view):
-        return not bool(request.user and request.user.is_authenticated)
