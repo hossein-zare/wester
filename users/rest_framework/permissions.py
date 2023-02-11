a21689a0a052a9d1e6b3ac8f90c7a1a5348d3a40
hossein-zare <zarehossein.hz1997@gmail.com>
2023-02-12 02:10:57 +0330

    Allow specifying methods for permission check.

--- users/rest_framework/permissions.py
+++ users/rest_framework/permissions.py
@@ -1,19 +1,5 @@
 from rest_framework.permissions import BasePermission
 
-class CanUser(BasePermission):
-    """
-    Check if the user can do the given action.
-    """
-
-    def __init__(self, action):
-        self.action = action
-
-    def has_permission(self, request, view):
-        return getattr(request.user.permission, self.action)
-
-    def __call__(self):
-        return self
-
 class IsGuest(BasePermission):
     """
     Allows access only to guest users (unauthenticated).
@@ -21,3 +7,21 @@ class IsGuest(BasePermission):
 
     def has_permission(self, request, view):
         return not bool(request.user and request.user.is_authenticated)
+
+class CanUser(BasePermission):
+    """
+    Check if the user can do the given action.
+    """
+
+    def __init__(self, action, methods=None):
+        self.action = action
+        self.methods = methods
+
+    def has_permission(self, request, view):
+        if not self.methods or request.method in self.methods:
+            return getattr(request.user.permission, self.action)
+
+        return True
+
+    def __call__(self):
+        return self
