33ff0e6650d1a4da004b79bb63053f8281c9ef1b
hossein-zare <zarehossein.hz1997@gmail.com>
2023-02-12 01:10:55 +0330

    Remove unused imports.

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,6 @@
 from django.urls import include, path, resolve
 
 from wester.utils import get_app_name
-from .apps import UserConfig
 
 urlpatterns = [
     path('api/' + get_app_name(__package__) + '/', include('users.rest_framework.urls')),
